699db98ae135b207c46cd707a9c399b6f545860a
Subhabrata Panda <111628898+subhabratapanda2000@users.noreply.github.com>
2022-09-18 10:05:18 +0530

    Add files via upload

---
+++ Middleware_SpringBoot_Java/Auth-Security-Management/src/test/java/microService/authManagement/AuthSecurityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package microService.authManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
